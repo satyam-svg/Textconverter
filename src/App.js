853b9b0097124d0b5b84169afc590edc459f43f5
Praveen <satyammaurya9620@gmail.com>
2023-08-24 15:37:47 +0530

    Integrate React Router for better user experience

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Textform from './components/Textform.js';
 import Alert from './components/Alert.js';
-// import About from './components/About.js';
+import About from './components/About.js';
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes, // Import Routes
+  Route,
+} from 'react-router-dom';
+
 function App() {
-  const [mode,setmode]=useState("light")
-  const [text,settext]=useState("Enable Dark mode");
-  const[alert,setalert]=useState(null);
-  const showalert=(message,type)=>{
-     setalert({
-      msg:message,
-      type:type
-  })
-  setTimeout(() => {
-    setalert(null);
-  }, 1000);
-  }
-const togglemode=()=>{
-  if(mode==='light'){
-    setmode('dark');
-    settext('Enable light mode');
-    document.body.style.backgroundColor='#343a40';
-    document.body.style.color='White';
-    showalert('Dark mode enabled','success')
-    document.title='Textconverter-Dark mode'
-  }else{
-    setmode('light');
-    settext('Enable dark mode');
-    document.body.style.backgroundColor='white';
-    document.body.style.color='black';
-    showalert('Light mode enabled','success')
-    document.title='Textconverter-Light mode'
-  }
-}
+  const [mode, setmode] = useState('light');
+  const [text, settext] = useState('Enable Dark mode');
+  const [alert, setalert] = useState(null);
+
+  const showalert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 1000);
+  };
+
+  const togglemode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      settext('Enable light mode');
+      document.body.style.backgroundColor = '#343a40';
+      document.body.style.color = 'White';
+      showalert('Dark mode enabled', 'success');
+      document.title = 'Textconverter-Dark mode';
+    } else {
+      setmode('light');
+      settext('Enable dark mode');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showalert('Light mode enabled', 'success');
+      document.title = 'Textconverter-Light mode';
+    }
+  };
+
   return (
     <>
-     <Navbar mode1={mode} text1={text} togglemode={togglemode} />
-     <Alert alert={alert}/>
-     <Textform title="Analyse your keywords" showalert={alert} />
-     {/* <About title="About Us"/> */}
-     </>
+      <Router>
+        <Navbar mode1={mode} text1={text} togglemode={togglemode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Textform title="Analyse your keywords" showalert={alert} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
